3e350bb4b2c35e8dbf0696c7b35fe22978b33fe9
Christy <appletoms99@outlook.com>
2020-10-25 16:49:59 -0500

    Step 3

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
 
+//adds middleware to load html, css and js files
+app.use("/", express.static(__dirname + "/public"));
+app.use("/css", express.static(__dirname + "/css"));
+app.use("/js", express.static(__dirname + "/js"));
+
+
 app.get('/heartbeat', (req, res) => {
     res.json({
-        is:working
+        is: "working"
     })
 });
 
